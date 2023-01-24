65059bb50da89c6aa17100e9f206ef46a6d69e8c
I551990 <constantin.heeren@sap.com>
2023-01-24 13:59:08 +0100

    funcltions

--- webservice-tutorial/main.go
+++ webservice-tutorial/main.go
@@ -13,4 +13,18 @@ func main() {
 		LastName:  "Smith",
 	}
 	fmt.Println(u)
+
+	port := 3000
+	_, err := startWebserver(port, 2)
+	fmt.Println(err)
+}
+
+// returns success or not (error) AND the port (int)
+func startWebserver(port, numberOfRetries int) (int, error) { // <name> <type>, ... or <name>, <name>, ... <type> if same type.
+	fmt.Println("Starting server...")
+	// do smth
+	fmt.Println("Server running on port", port)
+	fmt.Println("Number of retries:", numberOfRetries)
+	// return errors.New("Something went wrong when starting the server (test)")
+	return port, nil
 }
