bf232238e2c8526915999c505731d00b291deef4
I551990 <constantin.heeren@sap.com>
2023-02-01 19:34:51 +0100

    if and for

--- webservice-tutorial/models/user.go
+++ webservice-tutorial/models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	ID        int
 	FirstName string
@@ -16,8 +21,42 @@ func getUsers() []*User {
 }
 
 func addUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("User must not contain an ID or it must be set to 0.")
+	}
+
 	u.ID = nextID
 	nextID++
 	users = append(users, &u) // remember: users stores pointers, so we have to get the address of u!
 	return u, nil
+}
+
+func getUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+func updateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' not found", u.ID)
+}
+
+func removeUserByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("User with ID '%v' not found", id)
 }
